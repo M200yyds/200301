79b77d7217625c72345a0b0a8350d484482c2317
hymanHu <hujiangyx@163.com>
2020-06-02 15:23:50 +0800

    "2020-06-02, 搭建框架、第一个接口"

---
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.hqyj.SpringBootDemo.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	/**
+	 * 127.0.0.1:8080/test/desc
+	 */
+	@RequestMapping("/test/desc")
+	@ResponseBody
+	public String testDesc() {
+		return "This is test module desc.";
+	}
+}
