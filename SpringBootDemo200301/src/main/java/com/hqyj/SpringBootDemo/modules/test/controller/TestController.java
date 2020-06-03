40cfbbf62acb4327b3f91eb7985b059a28386197
hymanHu <hujiangyx@163.com>
2020-06-03 14:21:07 +0800

    "logback、多环境、热部署"

--- SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/controller/TestController.java
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.hqyj.SpringBootDemo.modules.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -24,6 +26,25 @@ public class TestController {
 	
 	@Autowired
 	private ApplicationTest applicationTest;
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+	
+	/**
+	 * 127.0.0.1/test/log
+	 */
+	@RequestMapping("/test/log")
+	@ResponseBody
+	public String logTest() {
+		// TRACE<DEBUG<INFO<WARN<ERROR
+		
+		LOGGER.trace("This is trace log");
+		LOGGER.debug("This is DEBUG log");
+		LOGGER.info("This is INFO log");
+		LOGGER.warn("This is WARN log");
+		LOGGER.error("This is ERROR log");
+		
+		return "This is log test.";
+	}
 	
 	/**
 	 * 127.0.0.1:8080/test/config
