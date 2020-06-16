d838498debbbefa9a86b52fc3c89ee8684a2a00f
hymanHu <hujiangyx@163.com>
2020-06-16 11:36:41 +0800

    "roles页面"

--- SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/account/service/impl/UserServiceImpl.java
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/account/service/impl/UserServiceImpl.java
@@ -38,7 +38,7 @@ public class UserServiceImpl implements UserService {
 	public Result<User> login(User user) {
 		User userTemp = userDao.getUserByUserName(user.getUserName());
 		if (userTemp == null || !userTemp.getPassword().equals(MD5Util.getMD5(user.getPassword()))) {
-			return new Result<User>(ResultStatus.FAILD.status, "User name or password error.");
+			return new Result<User>(ResultStatus.FAILED.status, "User name or password error.");
 		}
 		
 		return new Result<User>(ResultStatus.SUCCESS.status, "Login success.", userTemp);
@@ -61,7 +61,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public Result<Object> deleteUser(int userId) {
 		userDao.deleteUser(userId);
-		userRoleDao.deleteRolesByUserId(userId);
+		userRoleDao.deletUserRoleByUserId(userId);
 		return new Result<Object>(ResultStatus.SUCCESS.status, "Delete success.");
 	}
 
@@ -70,12 +70,12 @@ public class UserServiceImpl implements UserService {
 	public Result<User> editUser(User user) {
 		User userTemp = getUserByUserName(user.getUserName());
 		if (userTemp != null && userTemp.getUserId() != user.getUserId()) {
-			return new Result<User>(ResultStatus.FAILD.status, "User name is repeat.");
+			return new Result<User>(ResultStatus.FAILED.status, "User name is repeat.");
 		}
 
 		if (user.getUserId() > 0) {
 			userDao.updateUser(user);
-			userRoleDao.deleteRolesByUserId(user.getUserId());
+			userRoleDao.deletUserRoleByUserId(user.getUserId());
 		} else {
 			userDao.insertUser(user);
 		}
@@ -83,7 +83,7 @@ public class UserServiceImpl implements UserService {
 		List<Role> roles = user.getRoles();
 		if (roles != null && roles.size() > 0) {
 			for (Role role : roles) {
-				userRoleDao.insertUserRole(user.getUserId(), role.getRoleId());
+				userRoleDao.addUserRole(user.getUserId(), role.getRoleId());
 			}
 		}
 
